2c3a52aae86581190bf1444a94e7de043fa6c738
Igor Ferreira <me@igorcferreira.dev>
2026-02-24 14:12:54 +0100

    Fix code styling

--- CloudKitFeatureFlag/composeApp/src/main/java/dev/igorcferreira/cloudkitfeatureflag/Application.kt
+++ CloudKitFeatureFlag/composeApp/src/main/java/dev/igorcferreira/cloudkitfeatureflag/Application.kt
@@ -6,10 +6,10 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 
 class Application: Application() {
-override fun onCreate() {
-    super.onCreate()
-    DIHelper.initKoin()
-        .androidContext(applicationContext)
-        .androidLogger()
-}
+    override fun onCreate() {
+        super.onCreate()
+        DIHelper.initKoin()
+            .androidContext(applicationContext)
+            .androidLogger()
+    }
 }
